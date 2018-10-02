fc8fca7971ee5983ccac470fa827b7852bef6148
Matthew Cheney <m.cheney95@outlook.com>
2018-10-02 20:42:54 +0000

    commit

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
         $(document).ready(function() {
             var myurl = 'https://newsapi.org/v2/top-headlines?' +
-            'sources=the-new-york-times,bbc-news&' +
+            'sources=the-new-york-times,the-huffington-post,the-washington-post&' +
           'apiKey=84016fc1a1fb4b57b4d8e7950645ee27';
           console.log(myurl);
             $.ajax({
@@ -9,12 +9,18 @@
             success : function(parsed_json) {
                 console.log(parsed_json);
                 var title;
-                console.log(title);
-                var everything;
+                var sourceName;
+                var articleURL;
+                var everything = "";
                 var i;
-                for (i=0; i<19; i++) {
+                for (i=parsed_json['articles']['length']-1; i>-1; i--) {
                     title = parsed_json['articles'][i]['title'];
-                    everything += "<li>" + title + "</li>";
+                    sourceName = parsed_json['articles'][i]['source']['name'];
+                    articleURL = parsed_json['articles'][i]['url'];
+                    console.log(title);
+                    console.log(i);
+                    everything += "<li><h3><a href=\"" + articleURL + "\">" + title + "</a></h3>";
+                    everything += "<p>" + sourceName + "</p></li>"
                 }
                 $("#leftNewsList").html(everything);
                 
