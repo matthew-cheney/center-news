85d65e8e0bd088eb28ef2306e0aa9bdcfaef3573
Matthew Cheney <m.cheney95@outlook.com>
2018-10-03 20:46:55 +0000

    full content, all left news

--- javascript.js
+++ javascript.js
@@ -1,31 +1,35 @@
         $(document).ready(function() {
             var myurl = 'https://newsapi.org/v2/top-headlines?' +
-            'sources=the-new-york-times,the-huffington-post,the-washington-post&' +
-            'apiKey=84016fc1a1fb4b57b4d8e7950645ee27';
+                'sources=the-new-york-times,the-huffington-post,the-washington-post&' +
+                'apiKey=84016fc1a1fb4b57b4d8e7950645ee27';
             console.log(myurl);
             $.ajax({
-            url : myurl,
-            dataType : "json",
-            success : function(parsed_json) {
-                console.log(parsed_json);
-                var title;
-                var sourceName;
-                var articleURL;
-                var everything = "";
-                var i;
-                for (i=parsed_json['articles']['length']-1; i>-1; i--) {
-                    title = parsed_json['articles'][i]['title'];
-                    sourceName = parsed_json['articles'][i]['source']['name'];
-                    articleURL = parsed_json['articles'][i]['url'];
-                    console.log(title);
-                    console.log(i);
-                    everything += "<li><h3><a href=\"" + articleURL + "\" target=\"_blank\">" + title + "</a></h3>";
-                    everything += "<p>" + sourceName + "</p></li>"
+                url: myurl,
+                dataType: "json",
+                success: function(parsed_json) {
+                    console.log(parsed_json);
+                    var title;
+                    var sourceName;
+                    var articleURL;
+                    var everything = "";
+                    var i;
+                    for (i = parsed_json['articles']['length'] - 1; i > -1; i--) {
+                        title = parsed_json['articles'][i]['title'];
+                        sourceName = parsed_json['articles'][i]['source']['name'];
+                        articleURL = parsed_json['articles'][i]['url'];
+                        console.log(title);
+                        console.log(i);
+                        everything += "<li><h3><a href=\"" + articleURL + "\" target=\"_blank\">" + title + "</a></h3>";
+                        everything += "<p>" + sourceName + "</p></li>"
+                    }
+                    $("#leftNewsList").html(everything);
+
+                    $("#centerNewsList").html(everything);
+                    $("#rightNewsList").html(everything);
+
+
+
                 }
-                $("#leftNewsList").html(everything);
-                
-                
-                
-            }
             });
-          });
+        });
+        
